72eac18726320c0830aaa28737619af022eb5496
Pedro Estor Fernández <154912462+xDuwang86@users.noreply.github.com>
2024-03-05 14:22:04 +0100

    Agregados tests unitarios

--- ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/controller/HelloController.java
+++ ob-rest-datajpa/src/main/java/com/example/obrestdatajpa/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.example.obrestdatajpa.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HelloController {
 
